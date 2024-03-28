189ff75226f9e130b79c79faef7f185cf99f06e1
FelipePinzon <fepinzon77@gmail.com>
2024-03-28 11:39:43 -0500

    [ADD] .evn ready for db alone

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,9 +1,14 @@
 import connection from "@/app/libs/sql";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(request: NextRequest) {
    try {
     await connection;
+
+   return NextResponse.json({
+      message: "Melo caramelo"
+   })
+
    } catch (err){}
 }
 
