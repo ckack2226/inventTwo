6c82b6ad96d40e72ac3ec066e92fd8e834188cb6
ckack2226 <113803197+ckack2226@users.noreply.github.com>
2024-03-28 21:57:03 -0500

    avance front 1
    frontend-p1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,40 @@
+'use client'
+
+import { Form } from "./components/Forms"
 
 export default function LoginPage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Login</h1>
-    </main>
+    <>
+      <Form
+        title='Inicia Sesión'
+        onSubmit={() => {}}
+        description='Formulario para iniciar sesión'
+      >
+        <div className='my-[10px] flex flex-col gap-4'>
+          <Form.Input
+            label='Usuario'
+            name='Usuario'
+            placeholder='Ingresa Usuario'
+          />
+          <Form.Input
+            placeholder='Ingresa tu contraseña...'
+            label='Contraseña'
+            name='password'
+            type='password'
+          />
+        </div>
+        <Form.SubmitButton buttonText='Iniciar Sesión' />
+        <Form.Footer
+          description='Te olvidate tu contraseña?'
+          link='/forget-password'
+          textLink='Recuperar contraseña'
+        />
+        <Form.Footer
+          description='Aun no tienes cuenta?'
+          link='/register'
+          textLink='Registrate'
+        />
+      </Form>
+    </>
   )
 }
