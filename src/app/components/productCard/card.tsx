a8bac8135cb3542a903be098f93da1325445f8e1
ckack2226 <113803197+ckack2226@users.noreply.github.com>
2024-04-04 19:29:27 -0500

    boton activo
    redireccion para crud

--- src/app/components/productCard/card.tsx
+++ src/app/components/productCard/card.tsx
@@ -1,23 +1,28 @@
+import { useRouter } from "next/router";
 import "./card.scss";
+import Link from "next/link";
 
 interface ICard {
-    ProductoID:number,
-    Nombre:string,
-    Descripcion:string,
-    Precio:string,
-    UnidadMedida:string,
-    ProveedorID:number,
-    LicenciaCertificacionID?:number;
+    ProductoID: number,
+    Nombre: string,
+    Descripcion: string,
+    Precio: string,
+    UnidadMedida: string,
+    ProveedorID: number,
+    LicenciaCertificacionID?: number;
 }
 
-export default function Card(props: ICard){
-    return(
+export default function Card(props: ICard) {
+
+    return (
         <div className="card-container">
-            <span>
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="#000" xmlns="http://www.w3.org/2000/svg">
-            <path d='M14.06 9.02l.92.92L5.92 19H5v-.92l9.06-9.06M17.66 3c-.25 0-.51.1-.7.29l-1.83 1.83 3.75 3.75 1.83-1.83c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.2-.2-.45-.29-.71-.29zm-3.6 3.19L3 17.25V21h3.75L17.81 9.94l-3.75-3.75z' />
-            </svg>
-            </span>
+            <Link href={`/edit?product=${props.ProductoID}`}>
+                <span>
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="#000" xmlns="http://www.w3.org/2000/svg">
+                        <path d='M14.06 9.02l.92.92L5.92 19H5v-.92l9.06-9.06M17.66 3c-.25 0-.51.1-.7.29l-1.83 1.83 3.75 3.75 1.83-1.83c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.2-.2-.45-.29-.71-.29zm-3.6 3.19L3 17.25V21h3.75L17.81 9.94l-3.75-3.75z' />
+                    </svg>
+                </span>
+            </Link>
             <div className="info">
                 <h1>{props.Nombre}</h1>
                 <h3>{props.Descripcion}</h3>
