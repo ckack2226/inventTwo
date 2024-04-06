ba64be4c7822e4c976a6349a693a47b75d90d039
FelipePinzon <fepinzon77@gmail.com>
2024-04-05 20:50:23 -0500

    [ADD] Update form is OK on front but still no working to PUT on db

--- src/app/edit/page.tsx
+++ src/app/edit/page.tsx
@@ -11,6 +11,46 @@ export default function Edit() {
         <>
             <Header />
             <div>{searchParams.get('product')}</div>
+<form className="w-full max-w-lg">
+  <div className="flex flex-wrap -mx-3 mb-6">
+    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+        Nombre Producto
+      </label>
+      <input className="appearance-none block w-full bg-gray-200 text-gray-700 border rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white" type="text" name="Nombre" id="Nombre" placeholder="Nombre" />
+    </div>
+    <div className="w-full md:w-1/2 px-3">
+      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+        Precio
+      </label>
+      <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="number" name="Precio" id="Precio" placeholder="Precio" />
+    </div>
+  </div>
+  <div className="flex flex-wrap -mx-3 mb-6">
+    <div className="w-full px-3">
+      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+        Descripcion
+      </label>
+      <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="text" name="Descripcion" id="Descripcion" placeholder="Descripcion" />
+      <p className="text-gray-600 text-xs italic">La puedes hacer lo larga que quieras</p>
+    </div>
+  </div>
+  <div className="flex flex-wrap -mx-3 mb-2">
+    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+        Proveedor ID
+      </label>
+      <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="number" name="ProveedorID" id="ProveedorID" placeholder="Producto" />
+    </div>
+    <div className="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+      <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+        licencia Certificacion
+      </label>
+      <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" type="number" name="LicenciaCertificacionID" id="LicenciaCertificacionID" placeholder="Producto" />
+    </div>
+  </div>
+  <button type="submit" className="btn btn-primary">Actualizar Informacion Producto</button>
+</form>
         </>
     );
 }
